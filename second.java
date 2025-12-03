11b7ca17cd358f21ab881bf0f3478499e8aef3f7
Nigar khan <nigarp275@gmail.com>
2025-12-04 00:41:57 +0530

    rombus star pattern

--- second.java
+++ second.java
@@ -1,3 +1,15 @@
 public class second {
-    
+    public static void main(String[] args) {
+        int rows = 4;
+        int cols = 4;
+        for(int i=0;i<rows;i++){
+            for(int s=0;s<i;s++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<cols;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
